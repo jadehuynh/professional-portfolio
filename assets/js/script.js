58c7beba668f692414695ab104a4eb6d1aeb9bdf
jadehuynh <jadehuynh08@gmail.com>
2022-04-09 20:11:55 -0500

    redid the entire thing since the responsive design was not working with the previous format I had with bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+    function gameGit() {
+        window.location='https://github.com/jadehuynh/timed-game';
+    }
+    function gameDeploy() {
+        window.location='https://jadehuynh.github.io/timed-game/';
+    }
+    function weatherGit() {
+        window.location='https://github.com/jadehuynh/weather-app';
+    }
+    function weatherDeploy() {
+        window.location='https://jadehuynh.github.io/weather-app/';
+    }
+    function calenderGit() {
+        window.location='https://github.com/jadehuynh/calender-planner';
+    }
+    function calenderDeploy() {
+        window.location='https://jadehuynh.github.io/calender-planner/';
+    }
+    function projectGit() {
+        window.location='https://github.com/jadehuynh/project1-1';
+    }
+    function projectDeploy() {
+        window.location='https://midth002.github.io/project1/';
+    }
